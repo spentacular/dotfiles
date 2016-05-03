8d6b90c66f356e58633bd3e18282ac7ab507f41e
Spencer Hamm <spentacular@gmail.com>
2016-05-03 09:52:33 -0500

    updating hammerspoon bindings

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,61 +1,47 @@
-
+-- No animation
 hs.window.animationDuration = 0
 
 -- hotkeys
-local mash = {"cmd", "alt"}
-local winMash = {"shift", "alt"}
+local alt = {'alt'}
+local cmdAlt = {'cmd', 'alt'}
+local shiftAlt = {'shift', 'alt'}
+local cmdShift = {'cmd', 'shift'}
 
-----------------------------------------------------------------------------------
 -- Launch Applications
-
 local focusKeys = {
   a="Atom",
   c="Google Chrome",
   e="TextEdit",
+  m="Messages",
   s="Safari",
   t="iTerm"
 }
 
 for key in pairs(focusKeys) do
-  hs.hotkey.bind(mash, key, function()
+  hs.hotkey.bind(cmdAlt, key, function()
     hs.application.launchOrFocus(focusKeys[key])
   end)
 end
-----------------------------------------------------------------------------------
--- Half Window Bindings
 
-local halfWindowKeys = {
+-- Window Bindings
+local windowKeys = {
   h={x=0, y=0, w=0.5, h=1},
   j={x=0, y=0.5, w=1, h=0.5},
   k={x=0, y=0, w=1, h=0.5},
-  l={x=0.5, y=0, w=0.5, h=1}
+  l={x=0.5, y=0, w=0.5, h=1},
+  y={x=0, y=0, w=0.5, h=0.5},
+  u={x=0.5, y=0, w=0.5, h=0.5},
+  b={x=0, y=0.5, w=0.5, h=0.5},
+  n={x=0.5, y=0.5, w=0.5, h=0.5}
 }
 
-for key in pairs(halfWindowKeys) do
-  hs.hotkey.bind({"alt"}, key, function()
+for key in pairs(windowKeys) do
+  hs.hotkey.bind(alt, key, function()
     local win = hs.window.focusedWindow()
-    if win then win:moveToUnit(halfWindowKeys[key]) end
+    if win then win:moveToUnit(windowKeys[key]) end
   end)
 end
 
-----------------------------------------------------------------------------------
--- Quarter Corner Window Bindings
-
-local quarterCornerWindowKeys = {
-  ['1']={x=0, y=0, w=0.5, h=0.5},
-  ['2']={x=0.5, y=0, w=0.5, h=0.5},
-  ['3']={x=0, y=0.5, w=0.5, h=0.5},
-  ['4']={x=0.5, y=0.5, w=0.5, h=0.5}
-}
-
-for key in pairs(quarterCornerWindowKeys) do
-  hs.hotkey.bind({'alt'}, key, function()
-    local win = hs.window.focusedWindow()
-    if win then win:moveToUnit(quarterCornerWindowKeys[key]) end
-  end)
-end
-
-----------------------------------------------------------------------------------
 -- Quarter Column Window Bindings
 local quarterColWindowKeys = {
   ['1']={x=0, y=0, w=0.25, h=1},
@@ -65,27 +51,24 @@ local quarterColWindowKeys = {
 }
 
 for key in pairs(quarterColWindowKeys) do
-  hs.hotkey.bind(winMash, key, function()
+  hs.hotkey.bind(shiftAlt, key, function()
     local win = hs.window.focusedWindow()
     if win then win:moveToUnit(quarterColWindowKeys[key]) end
   end)
 end
 
-----------------------------------------------------------------------------------
--- Full Window Bindings
-hs.hotkey.bind({"alt", "shift"}, "Return", function()
+-- Full Width Window Bindings
+hs.hotkey.bind(shiftAlt, "Return", function()
   local win = hs.window.focusedWindow()
   if win then win:moveToUnit({x=0, y=0, w=1, h=1}) end
 end)
 
-----------------------------------------------------------------------------------
--- Eighth Center Window Bindings
-hs.hotkey.bind({"cmd", "shift"}, "Return", function()
+-- Center With Padding
+hs.hotkey.bind(cmdShift, "Return", function()
   local win = hs.window.focusedWindow()
   if win then win:moveToUnit({x=0.1, y=0.1, w=0.8, h=0.8}) end
 end)
 
-----------------------------------------------------------------------------------
 -- Network connection and disconnection
 local wifiWatcher = nil
 function ssidChangedCallback()
@@ -98,3 +81,9 @@ function ssidChangedCallback()
 end
 wifiWatcher = hs.wifi.watcher.new(ssidChangedCallback)
 wifiWatcher:start()
+
+-- Reload Config
+hs.hotkey.bind(cmdShift, "R", function()
+  hs.reload()
+end)
+hs.alert.show("Config loaded")
