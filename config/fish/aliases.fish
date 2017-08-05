bb110e92bdc7ab33255f7808c24afa2a23575509
Spencer Hamm <spentacular@gmail.com>
2017-08-05 01:21:34 -0500

    update aliases

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -13,17 +13,12 @@ alias d 'npm run dev'
 alias b 'npm run build'
 alias t 'npm run test'
 
-# General
-alias ya 'npm add'
-alias yd 'npm add --dev'
-alias yg 'npm global add'
-
 # Only view top level installs
 alias ng 'npm list -g --depth=0 2>/dev/null'
 alias nl 'npm list --depth=0 2>/dev/null'
 
 # Git Helpers
-alias g 'git'
+alias g 'hub'
 alias gs 'git status --short --branch'
 alias ga 'git add -A .'
 alias gc 'git commit'
@@ -33,9 +28,9 @@ alias gp 'git push'
 alias gl 'git pull'
 alias gt 'git tag'
 alias gm 'git merge'
-alias gco 'git checkout'
 alias gi 'gitignore'
+alias gco 'git checkout'
 
 # Apps
-function a ; atom-beta . ; end
+function a ; atom . ; end
 function c ; code . ; end
