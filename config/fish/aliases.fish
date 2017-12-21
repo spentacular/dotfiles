6ea133e41d743e6f322ab81182fb2d779dd14357
Spencer Hamm <spentacular@gmail.com>
2017-12-21 01:08:50 -0600

    updates

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -4,35 +4,30 @@ function ...   ; cd ../.. ; end
 function ....  ; cd ../../.. ; end
 function ..... ; cd ../../../.. ; end
 
-abbr cask 'brew cask'
+# Yarn
+abbr yr 'yarn run'
+abbr ys 'yarn start'
+abbr yd 'yarn run dev'
+abbr yb 'yarn run build'
+abbr yt 'yarn run test'
 
-# Scripts
-alias r 'yarn run'
-alias s 'yarn start'
-alias d 'yarn run dev'
-alias b 'yarn run build'
-alias t 'yarn run test'
-
-# Only view top level installs
-alias ng 'npm list -g --depth=0 2>/dev/null'
-alias nl 'npm list --depth=0 2>/dev/null'
-
-# Git Helpers
-abbr g 'hub'
-abbr gs 'git status --short --branch'
-abbr ga 'git add -A .'
+# Git
+alias g 'hub'
+alias gs 'git status --short --branch'
+alias ga 'git add -A .'
+alias gp 'git push'
+alias gl 'git pull'
+alias gm 'git merge'
+abbr gd 'git diff'
+abbr gi 'gitignore'
 abbr gc 'git commit'
 abbr gb 'git branch'
-abbr gd 'git diff'
-abbr gp 'git push'
-abbr gl 'git pull'
 abbr gt 'git tag'
-abbr gm 'git merge'
-abbr gi 'gitignore'
 abbr gco 'git checkout'
 
 # Misc
-alias m 'make'
+alias mit 'license-up mit Spencer Hamm spencerhamm.com'
+abbr cask 'brew cask'
 
 # Apps
 function a ; atom . ; end
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,9 @@
+# Go
+# set go workspace path
+set -xg GOPATH ~/.go
+# add the go bin path to be able to execute our programs
+set -x PATH $PATH /usr/local/opt/go/libexec/bin $GOPATH/bin
+
 # Set node_modules to the end of path
 # https://github.com/zeke/add-local-binaries-to-path
 set -xg PATH $PATH ./node_modules/.bin
